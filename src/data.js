5b47b6d10d2cd2b6ca8630b997199da6fa35ced7
Edwin Munene Riungu <eddiemitch262@gmail.com>
2022-10-07 18:12:52 +0300

    first cart commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "Mac Book Pro",
+      price: 1200,
+      image: "https://turtlesegg.com/images/detailed/14/3051461085.webp",
+    },
+    {
+      id: "2",
+      name: "Anon Mask",
+      price: 250,
+      image: "https://m.media-amazon.com/images/I/71cp3hVUmBL._AC_SL1500_.jpg",
+    },
+    {
+      id: "3",
+      name: "M7 Telescope",
+      price: 2500,
+      image:
+        "https://cdn11.bigcommerce.com/s-ufhcuzfxw9/images/stencil/500x659/products/13070/16561/Orion_SpaceProbe_II_76_mm_Altazimuth_Reflector_Telescope___51855.1506535242.jpg?c=2",
+    },
+  ],
+};
+export default data
